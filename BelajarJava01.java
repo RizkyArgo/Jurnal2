6016b22ee7396e2110a577443a7192d92e5a3c15
RizkyArgo <littleozil28@gmail.com>
2024-09-21 10:17:03 +0700

    Add files via upload

---
+++ BelajarJava01.java
@@ -0,0 +1,7 @@
+class BelajarJava01 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Rizky Argo Pradana");
+        System.out.println("NIM : 607062400005");
+        System.out.println("Kelas D3IF - 48 - 01");
+    }
+}
